bd2c2d069e61a9d89faadc1c62ed786f63b7e6c4
leetcode-notes <leetcode.notes@gmail.com>
2020-05-14 18:28:19 -0700

    Create 1436.py

---
+++ leetcode/1436.py
@@ -0,0 +1,10 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        counts = {}
+        for start, end in paths:
+            counts[start] = 1 if start not in counts else counts[start]+1
+            if end not in counts:
+                counts[end] = 0 
+        for k, v in counts.items():
+            if v == 0:
+                return k

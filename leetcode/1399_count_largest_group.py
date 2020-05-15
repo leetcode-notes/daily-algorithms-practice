eb59ca6c1691a4ba123fc09011358c8f6661db4d
leetcode-notes <leetcode.notes@gmail.com>
2020-05-15 01:09:20 -0700

    Create 1399_count_largest_group.py

---
+++ leetcode/1399_count_largest_group.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        groups = {}
+        for i in range(1, n+1):
+            d = self.digitSum(i)
+            groups[d] = 1 if d not in groups else groups[d]+1
+        largest = 0 
+        count = 0
+        for k, v in groups.items():
+            if v > largest:
+                count = 1 
+                largest = v 
+            elif v == largest:
+                count += 1 
+        return count 
+            
+    
+    def digitSum(self, num):
+        n = str(num)
+        total = 0
+        for c in n:
+            total += int(c)
+        return total

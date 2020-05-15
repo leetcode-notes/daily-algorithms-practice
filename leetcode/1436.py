235ad5951d52dde7cc0b02c7a22d5063402984d6
leetcode-notes <leetcode.notes@gmail.com>
2020-05-14 19:36:54 -0700

    fix type hinting

--- leetcode/1436.py
+++ leetcode/1436.py
@@ -1,18 +1,20 @@
-from typing import List
+from typing import Dict, List, Any
+
 
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        counts = {}
+        counts: Dict[str, Any] = {}
         for start, end in paths:
             counts[start] = 1 if start not in counts else counts[start]+1
             if end not in counts:
                 counts[end] = 0 
         for k, v in counts.items():
             if v == 0:
-                return k 
+                return k
+        return "None"
 
 
-paths = [["B","C"],["D","B"],["C","A"]]
+paths = [["B", "C"], ["D", "B"], ["C", "A"]]
 
 s = Solution()
 print(s.destCity(paths))

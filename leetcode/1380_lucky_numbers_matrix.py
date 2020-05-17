1e34db7824b16df7f5a63db6ad683c66c0f28b15
leetcode-notes <leetcode.notes@gmail.com>
2020-05-16 20:53:17 -0700

    linting

--- leetcode/1380_lucky_numbers_matrix.py
+++ leetcode/1380_lucky_numbers_matrix.py
@@ -8,15 +8,15 @@ class Solution:
         for i, row in enumerate(matrix):
             row_min[i] = min(row)
         for j in range(len(matrix[0])):
-            cur_max = 0 
+            cur_max = 0
             for row in matrix:
                 if row[j] > cur_max:
                     cur_max = row[j]
-            col_max[j] = cur_max 
+            col_max[j] = cur_max
         res = []
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 cur_num = matrix[i][j]
                 if cur_num == col_max[j] and cur_num == row_min[i]:
                     res.append(cur_num)
-        return res               
+        return res
---
+++ leetcode/1396_design_underground_system.py
@@ -0,0 +1,20 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        pass
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        pass
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        pass
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        pass
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)

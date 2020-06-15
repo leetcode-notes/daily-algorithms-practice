3329d28347f4fac8c93a90050cbe1525d7a52f1d
leetcode-notes <leetcode.notes@gmail.com>
2020-06-14 22:11:30 -0700

    add brick wall

---
+++ leetcode/554_brick_wall.py
@@ -0,0 +1,43 @@
+# keep track of number of "cracks" at each column
+from typing import List
+
+
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        d = {}
+        for row in wall:
+            start = 0
+            for i, v in enumerate(row):
+                if i == len(row)-1:
+                    continue
+                start += v
+                d[start] = 1 if start not in d else d[start]+1
+        if not d:
+            return len(wall)
+        max_gaps = max(d.values())
+        return len(wall) - max_gaps
+
+
+test1 = [[1, 2, 2, 1],
+         [3, 1, 2],
+         [1, 3, 2],
+         [2, 4],
+         [3, 1, 2],
+         [1, 3, 1, 1]]
+
+s = Solution()
+print(s.leastBricks(test1))
+
+
+test2 = [[1], [1], [1]]
+
+'''
+Success
+Details 
+Runtime: 200 ms, faster than 51.07% of Python3 online submissions for Brick Wall.
+Memory Usage: 19 MB, less than 5.60% of Python3 online submissions for Brick Wall.
+Next challenges:
+Max Points on a Line
+Rearrange String k Distance Apart
+Daily Temperatures
+'''

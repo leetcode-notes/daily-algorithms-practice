1e6b910552cb50e0c5c111666957a8830923fe6c
leetcode-notes <leetcode.notes@gmail.com>
2020-06-14 22:11:46 -0700

    fix

--- cscacademy/max_subarray_even.py
+++ cscacademy/max_subarray_even.py
@@ -4,13 +4,17 @@ length.
 '''
 
 
-def max_subarray_even_length(arr) -> int:
+def max_subarray_even_length(arr):
     n = len(arr)
     dp = [0] * (n+2)
-    res = int('-inf')
+    res = -float('inf')
     for i in range(1, n):
         dp[n-i-1] = arr[n-i-1] + arr[n-i] + dp[n-i+1]
         if dp[n-i-1] > res:
             res = dp[n-i-1]
         dp[n-i-1] = max(0, dp[n-i-1])
+    print(dp)
     return res
+
+
+print(max_subarray_even_length([8, 9, -8, 9, 10]))

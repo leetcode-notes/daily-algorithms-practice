2fb9bf95aea31162c8fdf23deca3d4e2f0ff8580
leetcode-notes <leetcode.notes@gmail.com>
2020-06-20 23:38:23 -0700

    add 12 integer to roman

---
+++ leetcode/12_integer_to_roman.py
@@ -0,0 +1,46 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        thous, hundreds, tens, ones = 0, 0, 0, 0
+        thous = num // 1000
+        hundreds = num // 100 % 10
+        tens = num // 10 % 10
+        ones = num % 10
+        res = []
+        if thous > 0:
+            while thous > 0:
+                res.append("M")
+                thous -= 1
+        if hundreds == 9:
+            res.append("CM")
+            hundreds -= 9
+        if hundreds >= 5:
+            res.append("D")
+            hundreds -= 5
+        if hundreds == 4:
+            res.append("CD")
+            hundreds -= 4
+        for _ in range(hundreds):
+            res.append("C")
+        if tens == 9:
+            res.append("XC")
+            tens -= 9
+        if tens >= 5:
+            res.append("L")
+            tens -= 5
+        if tens == 4:
+            res.append("XL")
+            tens -= 4
+        for _ in range(tens):
+            res.append("X")
+        if ones == 9:
+            res.append("IX")
+            ones -= 9
+        if ones >= 5:
+            res.append("V")
+            ones -= 5
+        if ones == 4:
+            res.append("IV")
+            ones -= 4
+        for _ in range(ones):
+            res.append("I")
+        return "".join(res)

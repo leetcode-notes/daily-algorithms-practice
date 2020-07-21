a11a9d1a75779c82df0d7edd498ba79cb3cb612f
leetcode-notes <leetcode.notes@gmail.com>
2020-07-20 17:26:44 -0700

    add union by rank DSU

---
+++ library/dsu.py
@@ -0,0 +1,20 @@
+class DSU:
+    def __init__(self, n):
+        self.par = list(range(n))
+        self.rnk = [0] * n
+
+    def find(self, x):
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def union(self, x, y):
+        xr, yr = self.find(x), self.find(y)
+        if xr == yr:
+            return False
+        if self.rnk[xr] < self.rnk[yr]:
+            xr, yr = yr, xr
+        if self.rnk[xr] == self.rnk[yr]:
+            self.rnk[xr] += 1
+        self.par[yr] = xr
+        return True

8ccd2dc98b53faf062b38d31bcb8fe3eab48ed1d
leetcode-notes <leetcode.notes@gmail.com>
2020-07-28 11:22:54 -0700

    add dsu

--- library/dsu.py
+++ library/dsu.py
@@ -1,3 +1,31 @@
+class Solution:
+    def findKthLargest(self, nums, k):
+        return quick_select(nums, 0, len(nums)-1, k-1)
+
+
+def quick_select(nums, l, r, k):
+    if l > r:
+        return nums[k]
+    s, e = l, r
+    pi = random.randint(l, r)
+    pivot = nums[pi]
+    while l <= r:
+        while l <= r and nums[r] < pivot:
+            r -= 1
+        while l <= r and nums[l] > pivot:
+            l += 1
+        if l <= r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+    if s <= k <= r:
+        return quick_select(nums, s, r, k)
+    elif l <= k <= e:
+        return quick_select(nums, l, e, k)
+    else:
+        return nums[k]
+
+
 '''
 Union by rank with path compression O()
 O(m log* n), amortized to be O(log*n), where m is number of operations, n is number of elements
